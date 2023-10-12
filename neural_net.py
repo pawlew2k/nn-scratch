7021690b46a919426836b2ba5829f49067cffc87
Adam Skaskiewicz <skaskiewiczadam@gmail.com>
2023-10-12 13:38:16 +0200

    trying to see where the bugs are

--- neural_net.py
+++ neural_net.py
@@ -23,17 +23,27 @@ class Layer:
     def __init__(self, in_size: int, out_size: int, activ_func: str):
         self.activ_func = ACTIVATION_FUNCTION_DICT.get(activ_func, ACTIVATION_FUNCTION_DICT["RELU"])
         self.activ_func_deriv = ACTIVATION_FUNCTION_DERIVATIVE_DICT.get(activ_func,
-                                                      ACTIVATION_FUNCTION_DERIVATIVE_DICT["RELU"])
+                                                                        ACTIVATION_FUNCTION_DERIVATIVE_DICT["RELU"])
 
-        # bias as last value in weights => [weights, bias]
+        ## bias as last value in weights => [weights, bias]
         weight_heuristic = WEIGHT_HEURISTICS.get(activ_func, WEIGHT_HEURISTICS["RELU"])
         self.weights = np.random.randn(in_size + 1, out_size) * weight_heuristic(in_size, out_size)
 
-        self.inputs = np.zeros(in_size)
-        self.outputs = np.zeros(out_size)
-        self.delta = np.array([0])
+        ### TESTING DATA
+        # if out_size == 2:
+        #     weights = np.array([[1.0, 3.0], [2.0, 4.0], [5.0, 6.0]])
+        # else:
+        #     weights = np.array([[2.0], [1.0], [3.0]])
+        #
+        # self.weights = weights
 
-        # self.bias = np.random.random(out_size) * weight_heuristic
+        # output before activation function
+        self.before_activation = np.zeros(in_size)
+        # output after activation function
+        self.outputs = np.zeros(out_size)
+
+        # deltas for layer
+        self.delta = np.array([0])
 
     def __str__(self):
         w = self.get_weights()
@@ -41,10 +51,11 @@ class Layer:
         return '\n'.join([f'W_{i}={w[i]}, b_{i}={b[i]}' for i in range(w.shape[0])])
 
     def get_weights(self):
-        return self.weights[:, :-1]
+        return self.weights[:-1, :]
 
     def get_biases(self):
-        return self.weights[:, -1]
+        return self.weights[-1, :]
+
 
 class NeuralNet:
     def __init__(self, layers: list[(int, str)], loss_func: str, rate: float = 0.1, seed: int = 42):
@@ -73,9 +84,9 @@ class NeuralNet:
 
                 # Feed forward
                 for layer in self.layers:
-                    layer.inputs = out
                     out = np.append(out, 1.0)
-                    out = layer.activ_func(out.dot(layer.weights))
+                    layer.before_activation = out.dot(layer.weights)
+                    out = layer.activ_func(layer.before_activation)
                     layer.outputs = out
 
                 # print(out)
@@ -86,14 +97,20 @@ class NeuralNet:
                 print(f"loss on epoch {epoch}: {loss}")
 
                 loss_derivative = self.loss_deriv(np.array(target), out)
-                self.layers[-1].delta = loss_derivative*self.layers[-1].activ_func_deriv(self.layers[-1].inputs)
-                self.layers[-1].weights -= self.learning_rate * self.layers[-1].outputs.T.dot(self.layers[-1].delta)
+                deriv = self.layers[-1].activ_func_deriv(self.layers[-1].before_activation)
+                self.layers[-1].delta = loss_derivative * deriv
+                outputs_T = self.layers[-1].outputs.T
+                dotted = outputs_T.dot(self.layers[-1].delta)
+                change = self.learning_rate * dotted
+                self.layers[-1].weights -= change
 
-                for i in range(len(self.layers) - 2, 0, -1):
-                    delta = self.layers[i+1].delta.dot(self.layers[i].weights.T)
-                    self.layers[i].delta = delta * self.layers[i].activ_func_deriv(self.layers[i].inputs)
+                for i in range(len(self.layers) - 2, -1, -1):
+                    weights_t = self.layers[i].weights.T
+                    delta_prev_layer = self.layers[i + 1].delta
+                    dot = delta_prev_layer.dot(weights_t)
+                    self.layers[i].delta = dot * self.layers[i].activ_func_deriv(self.layers[i].before_activation) # TODO: tutaj cos nie dziala nadal
+
                     self.layers[i].weights -= self.learning_rate * self.layers[i].outputs.T.dot(self.layers[i].delta)
-
 
     def predict(self, data: list[list[float]]):
         # predict the outcome
@@ -103,4 +120,8 @@ class NeuralNet:
 if __name__ == '__main__':
     net = NeuralNet([(3, ""), (2, "LINEAR"), (1, "LINEAR")], "MSE")
     net.train(1, [[1.0, 2.0, 3.0]], [[1.0]])
+
+    ### TESTING DATA
+    # net = NeuralNet([(2, ""), (2, "LINEAR"), (1, "LINEAR")], "MSE")
+    # net.train(1, [[10, 20]], [[1.0]])
     print(net)
