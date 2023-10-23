bug fix get_weights/gradient, visualize flattened weights/gradients

utils/weights_visualizator/VisualizeNN.py
@@ -79,7 +79,7 @@ class Layer():
         linewidth = np.log(abs_weight)
 
         # draw the weights and adjust the labels of weights to avoid overlapping
-        if abs_weight > 0.5:
+        if textoverlaphandler:
             # while loop to determine the optimal locaton for text lables to avoid overlapping
             index_step = 2
             num_segments = 10
@@ -96,7 +96,7 @@ class Layer():
                             neuron2.y - neuron1.y + 2 * y_adjustment) / num_segments
 
             # print("Label positions: ", "{:.2f}".format(txt_x_pos), "{:.2f}".format(txt_y_pos), "{:3.2f}".format(weight))
-            a = pyplot.gca().text(txt_x_pos, txt_y_pos, "{:3.2f}".format(weight), size=8, ha='center')
+            a = pyplot.gca().text(txt_x_pos, txt_y_pos, "{:.4}".format(weight), size=8, ha='center')
             a.set_bbox(dict(facecolor='white', alpha=0))
             # print(a.get_bbox_patch().get_height())
 
@@ -162,13 +162,16 @@ class NeuralNetwork():
         layer = Layer(self, number_of_neurons, self.number_of_neurons_in_widest_layer)
         self.layers.append(layer)
 
-    def draw(self, weights_list=None):
-        # vertical_distance_between_layers and horizontal_distance_between_neurons are the same with the variables of the same name in layer class
-        vertical_distance_between_layers = 6
-        horizontal_distance_between_neurons = 2
-        overlaphandler = TextOverlappingHandler( \
-            self.number_of_neurons_in_widest_layer * horizontal_distance_between_neurons, \
-            len(self.layers) * vertical_distance_between_layers, grid_size=0.2)
+    def draw(self, weights_list=None, show_values: bool = False):
+        if show_values:
+            # vertical_distance_between_layers and horizontal_distance_between_neurons are the same with the variables of the same name in layer class
+            vertical_distance_between_layers = 6
+            horizontal_distance_between_neurons = 2
+            overlaphandler = TextOverlappingHandler( \
+                self.number_of_neurons_in_widest_layer * horizontal_distance_between_neurons, \
+                len(self.layers) * vertical_distance_between_layers, grid_size=0.2)
+        else:
+            overlaphandler = None
 
         pyplot.figure(figsize=(12, 9))
         for i in range(len(self.layers)):
@@ -189,7 +192,7 @@ class DrawNN():
     # from input layer to output layer, e.g., a neural network of 5 nerons in the input layer, 
     # 10 neurons in the hidden layer 1 and 1 neuron in the output layer is [5, 10, 1]
     # para: weights_list (optional) is the output weights list of a neural network which can be obtained via classifier.coefs_
-    def __init__(self, neural_network, weights_list=None):
+    def __init__(self, neural_network, weights_list=None, show_values: bool = False):
         self.neural_network = neural_network
         self.weights_list = weights_list
         # if weights_list is none, then create a uniform list to fill the weights_list
@@ -199,13 +202,14 @@ class DrawNN():
                 tempArr = np.ones((first, second)) * 0.4
                 weights_list.append(tempArr)
             self.weights_list = weights_list
+        self.show_values = show_values
 
     def draw(self, savefig=False, path: str = None):
         widest_layer = max(self.neural_network)
         network = NeuralNetwork(widest_layer)
         for l in self.neural_network:
             network.add_layer(l)
-        network.draw(self.weights_list)
+        network.draw(self.weights_list, self.show_values)
         if savefig:
             if path:
                 dir = os.path.dirname(path)

visualization/visualizer.py
@@ -44,15 +44,25 @@ class Visualizer:
             plt.show()
 
     @staticmethod
-    def show_net_weights(net: NeuralNet, savefig: bool = False, path: str = None):
+    def show_net_weights(net: NeuralNet, savefig: bool = False, path: str = None, flatten: bool = False):
         weights = [layer.get_weights() for layer in net.layers]
-        network = VisNN.DrawNN(net.net_structure, weights)
+        if not flatten:
+            network = VisNN.DrawNN(net.net_structure, weights, show_values=flatten)
+        else:
+            flat_weights = [np.array([[np.sqrt(np.sum(weight ** 2))]]) for weight in weights]  # L2 norm
+            net_structure = [1 for _ in range(len(net.net_structure))]
+            network = VisNN.DrawNN(net_structure, flat_weights, show_values=flatten)
         network.draw(savefig, path)
 
     @staticmethod
-    def show_gradients(net: NeuralNet, savefig: bool = False, path: str = None):
+    def show_gradients(net: NeuralNet, savefig: bool = False, path: str = None, flatten: bool = False):
         gradients = [layer.get_gradient() for layer in net.layers]
-        network = VisNN.DrawNN(net.net_structure, gradients)
+        if not flatten:
+            network = VisNN.DrawNN(net.net_structure, gradients, show_values=flatten)
+        else:
+            flat_gradients = [np.array([[np.sqrt(np.sum(gradient ** 2))]]) for gradient in gradients]  # L2 norm
+            net_structure = [1 for _ in range(len(net.net_structure))]
+            network = VisNN.DrawNN(net_structure, flat_gradients, show_values=flatten)
         network.draw(savefig, path)
 
     @staticmethod
@@ -176,11 +186,11 @@ if __name__ == '__main__':
 
     # train neural network
     print("[INFO] training network...")
-    # for i in range(10):
-    #     model.train(x, sigmoid_normalized, epochs=10, learning_rate=0.01)
+    for i in range(1):
+        model.train(x, sigmoid_normalized, epochs=10, learning_rate=0.01)
     # print(net)
     # print(net.net_structure)
-    # Visualizer.show_gradients(model)
+    Visualizer.show_net_weights(model, flatten=False)
     model.train(x, sigmoid_normalized, epochs=100, learning_rate=0.01)
     test_data = Dataset(path='../datasets/projekt1/regression/data.cube.test.1000.csv')
     Visualizer.show_prediction(model, test_data, savefig=True, path='../plots/predict_regression/example.jpg')