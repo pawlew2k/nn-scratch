d7f2c73661af997cbdb06e592da056359fa5da5e
Adam Skaskiewicz <skaskiewiczadam@gmail.com>
2023-10-24 21:40:23 +0200

    working classification

--- tests/mnist_tests.py
+++ tests/mnist_tests.py
@@ -17,31 +17,26 @@ def mnist_classification():
     data = digits.data.astype("float")
     data = (data - data.min()) / (data.max() - data.min())
 
-    # construct the training and testing splits
-    (trainX, testX, trainY, testY) = train_test_split(data,
+    (train_x, test_x, train_y, test_y) = train_test_split(data,
                                                       digits.target, test_size=0.25)
-    # convert the labels from integers to vectors
-    trainY = LabelBinarizer().fit_transform(trainY)
-    testY = LabelBinarizer().fit_transform(testY)
+
+    train_y = LabelBinarizer().fit_transform(train_y)
+    test_y = LabelBinarizer().fit_transform(test_y)
 
     # train the network
     print("[INFO] training network...")
-    model = NeuralNet([(trainX.shape[1], ""), (32, SIGMOID), (16, SIGMOID), (10, SOFTMAX)], CROSS_ENTROPY)
-    print("[INFO] {}".format(model))
+    model = NeuralNet([(train_x.shape[1], ""), (32, SIGMOID), (16, SIGMOID), (10, SOFTMAX)], CROSS_ENTROPY)
 
-    start = time.time()
-    model.train(trainX, trainY, epochs=5000, learning_rate=0.005)
-    end = time.time()
-    print(f"elapsed time {end - start}")
+    model.train(train_x, train_y, epochs=2000, learning_rate=0.01)
 
     # save model
     serialize_model(model, '/models/mnist/mnist.json')
 
     # evaluate the network
     print("[INFO] evaluating network...")
-    prepare_test_x = np.atleast_2d(testX)
+    prepare_test_x = np.atleast_2d(test_x)
     prepare_test_x = np.c_[prepare_test_x, np.ones((prepare_test_x.shape[0]))]
 
     predictions = model.predict(prepare_test_x)
     predictions = predictions.argmax(axis=1)
-    print(classification_report(testY.argmax(axis=1), predictions, digits=4))
+    print(classification_report(test_y.argmax(axis=1), predictions, digits=4))
