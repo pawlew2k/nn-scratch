bddd65dd17f6b51eb1e0422b7e0072bc8d154341
Adam Skaskiewicz <skaskiewiczadam@gmail.com>
2023-10-24 22:06:33 +0200

    commented mnist learning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tests.mnist_tests import mnist_classification, mnist_decode_and_predict
 
 if __name__ == '__main__':
-    mnist_classification()
+    # mnist_classification()
     mnist_decode_and_predict()
